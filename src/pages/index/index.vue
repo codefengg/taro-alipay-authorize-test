e8afced22dda031dfba31447f74f8b7e4c2f5854
PatrickStar <1060558028@qq.com>
2022-04-20 15:01:46 +0800

    项目初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <view class="index">
+    <view>
+      <img src="" alt="">
+    </view>
+    {{ msg }}
+    <view class="btn">
+      <nut-button type="primary" @click="handleClick('text', msg2, true)">点我</nut-button>
+    </view>
+    <nut-toast :msg="msg" v-model:visible="show" :type="type" :cover="cover"/>
+    <button
+        scope='userInfo'
+        open-type="getAuthorize"
+        :onGetAuthorize="handleGetUserInfo"
+        :onError="handleAliPayAuthorizeError"
+    >支付宝获取用户信息
+    </button>
+  </view>
+</template>
+
+<script>
+import {reactive, toRefs} from 'vue';
+
+export default {
+  name: 'Index',
+  components: {},
+  setup() {
+    const state = reactive({
+      msg: '欢迎使用 NutUI3.0 开发小程序',
+      msg2: '你成功了～',
+      type: 'text',
+      show: false,
+      cover: false
+    });
+
+    const handleClick = (type, msg, cover = false) => {
+      state.show = true;
+      state.msg2 = msg;
+      state.type = type;
+      state.cover = cover;
+    };
+
+    const handleGetUserInfo = (event) => {
+      console.log('用户信息========', event)
+    }
+
+    const handleAliPayAuthorizeError = (error) => {
+      console.log('error==========', error)
+    }
+
+    return {
+      ...toRefs(state),
+      handleClick,
+      handleGetUserInfo,
+      handleAliPayAuthorizeError
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.index {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
